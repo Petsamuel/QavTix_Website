023bdec5789a2a66e4cb2e455dabfacc13677607
Nelson Onuegbu <leonenelson38@gmail.com>
2026-04-26 00:38:13 +0100

    feat: implement event details preview component, define event types, and set up event details container layout

--- src/components/events-page/EventDetailsPageContentContainer.tsx
+++ src/components/events-page/EventDetailsPageContentContainer.tsx
@@ -6,6 +6,7 @@ import EventOverviewSection from "./event-details/EventOverviewSection"
 import HostNAttendeeDetailsSection from "./event-details/HostNAttendeeSection"
 import RelatedEventsYouMightLike from "./event-details/RelatedEventsYouMightLike"
 import ContactHostForm from "../forms/host/ContactHostForm"
+import { Skeleton } from "../ui/skeleton"
 
 
 interface Props {
@@ -14,6 +15,7 @@ interface Props {
 }
 
 export default function EventDetailsPageContentContainer({ event, relatedEvents }: Props) {
+    console.log(event)
     return (
         <main className="pb-20">
             <SectionHeading title="Events" />
@@ -23,13 +25,17 @@ export default function EventDetailsPageContentContainer({ event, relatedEvents 
                 {/* Left column — desktop only */}
                 <div className="hidden md:block md:w-[45%]">
                     <figure>
-                        <Image
-                            src="/images/demo-images/event-detail-img.png"
-                            alt={event.title}
-                            width={900}
-                            height={900}
-                            className="rounded-4xl h-60 object-cover md:h-96"
-                        />
+                        {event.event_media.length > 0 ?
+                            <Image
+                                src={event.event_media.find(v => v.is_featured)?.image_url || event.event_media[0].image_url}
+                                alt={event.title}
+                                width={900}
+                                height={900}
+                                className="rounded-4xl h-60 object-cover md:h-96"
+                            />
+                            :
+                            <Skeleton className="rounded-4xl h-60 object-cover md:h-96" />
+                        }
                     </figure>
                     <HostNAttendeeDetailsSection event={event} className="md:mt-8" />
                     <ContactHostForm event={event} />
@@ -39,13 +45,17 @@ export default function EventDetailsPageContentContainer({ event, relatedEvents 
                 <section className="mt-12 md:mt-0 md:w-[50%]">
                     {/* Mobile image */}
                     <figure className="md:hidden">
-                        <Image
-                            src={event.event_media.image_url}
-                            alt={event.title}
-                            width={900}
-                            height={900}
-                            className="rounded-4xl h-[50%] max-w-full w-[32em] mx-auto object-cover"
-                        />
+                        {event.event_media.length > 0 ?
+                            <Image
+                                src={event.event_media.find(v => v.is_featured)?.image_url || event.event_media[0].image_url}
+                                alt={event.title}
+                                width={900}
+                                height={900}
+                                className="rounded-4xl h-[50%] max-w-full w-[32em] mx-auto object-cover"
+                            />
+                            :
+                            <Skeleton className="rounded-4xl h-[50%] max-w-full w-[32em] mx-auto object-cover" />
+                        }
                     </figure>
 
                     <EventOverviewSection event={event} />
